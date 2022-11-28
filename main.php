37d431e1cbb0cbfe566fd027434d52c2dffd352e
eastsidekillas <68759285+eastsidekillas@users.noreply.github.com>
2022-11-28 19:29:16 +0500

    New version

---
+++ main.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="ru" class="translated-ltr"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>EASTSIDEKILLAS</title>
+		
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+		<style>
+			body {
+				background-color: #000000;
+				margin: 0;
+				padding: 0;
+				overflow: hidden;
+			}
+
+			* {
+            	margin: 0;
+            	padding: 0;
+
+				font-family: monospace;
+				color: white;
+				text-shadow: 2px 2px #000;
+			}
+
+			.center {
+				position: fixed;
+				top: 50%;
+				left: 50%;
+
+				transform: translate(-50%, -50%);
+				color: white;
+				text-shadow: 2px 2px #000;
+        	}
+			
+			.info {
+				z-index: 999999;
+				color: white;
+			}
+
+			.begin {
+				cursor: pointer;
+			}
+
+			#overlay {
+				position: fixed;
+				z-index: 9999;
+				background-color: #000;
+
+				top: 0px;
+				left: 0px;
+				width: 100%;
+				height: 100%;
+        	}
+
+			#swarm {
+				display: block;
+			}
+
+			.marquee {
+				width: 450px;
+				line-height: 50px;
+				color: white;
+				white-space: nowrap;
+				overflow: hidden;
+				box-sizing: border-box;
+			}
+			
+			.marquee p {
+				display: inline-block;
+				padding-left: 100%;
+				animation: marquee 15s linear infinite;
+			}
+
+			@keyframes marquee {
+				0%   { transform: translate(0, 0); }
+				100% { transform: translate(-100%, 0); }
+			}
+		</style>
+	<link type="text/css" rel="stylesheet" charset="UTF-8" href="/core/css/translateelement.css"></head>
+	<body style="padding: 0; margin: 0;">
+		<canvas id="swarm" width="1920" height="880"></canvas>
+
+   <div style="border-width: thin; width: 100%; position: absolute; top: 0px; left: 0px; text-align: right;">
+   </div>
+        <div style="font-size: 40px;" class="logo-blur logo-bass"><span class="logo-neon"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">EASTSIDEKILLAS</font></font></span></div>                                        
+
+		<div class="center" id="data" style="opacity: 1.08347; margin-top: 120px; text-align: center; display: block;">
+			<a href="https://vk.com/audios266909294?section=all&z=audio_playlist266909294_123"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">full pack razieb</font></font></a>
+			<a href="https://vk.com/eastsidekillas"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">VK </font></font></a>
+			<a href="t.me/eastsidekillas"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Telegram</font></font></a>
+			<br> <br> <pre>social links</pre>
+			<br> <br> <pre> </pre>
+		</div>
+		<div class="center" id="shoutout_marquee" style="opacity: 1.08347; margin-top: 155px; display: block;">
+			<div class="marquee"> 
+				<p>Full stack web developer</p>
+			</div>
+		</div>
+		<audio id="music" src="/core/mp3/FUTXRE CULTXRE - FEEL THE PRESSURE UNTIL THE END.mp3" autoplay="" loop="" onplaying="setTimeout(()=&gt;{window.overlay.remove();render();start_text();},3500);" style="display: none;"></audio>
+		<script src="/core/js/page.js"></script>
+		<script src="/core/js/title.js"></script>
